c00e7ac130bfa0a3d03a430163fbc748258491c9
Shane D Wilson <me@shanedwilson.com>
2019-07-15 18:26:56 -0500

    wip

---
+++ WrenchIt/Models/CreateMachineRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WrenchIt.Models
+{
+    public class CreateMachineRequest
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Trim { get; set; }
+        public int Type { get; set; }
+        public string OilType { get; set; }
+        public int OilQuantity { get; set; }
+        public int MyProperty { get; set; }
+        public string TireSize { get; set; }
+        public int TirePressure { get; set; }
+        public int ServiceInterval { get; set; }
+    }
+}
---
+++ WrenchIt/Models/Machine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WrenchIt.Models
+{
+    public class Machine    
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Trim { get; set; }
+        public int Type { get; set; }
+        public string OilType { get; set; }
+        public int OilQuantity { get; set; }
+        public string TireSize { get; set; }
+        public int TirePressure { get; set; }
+        public int ServiceInterval { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

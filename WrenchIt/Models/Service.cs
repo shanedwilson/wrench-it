e7c5bb24c9f56e1f4b9f92141704301439d9270f
Shane D Wilson <me@shanedwilson.com>
2019-07-16 01:14:43 -0500

    service models created

---
+++ WrenchIt/Models/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WrenchIt.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public int UserMachineId { get; set; }
+        public int Mileage { get; set; }
+        public DateTime ServiceDate { get; set; }
+        public string Notes { get; set; }
+    }
+}

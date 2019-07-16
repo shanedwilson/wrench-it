070a76e357968ec843575b898bc9265d205852ab
Shane D Wilson <me@shanedwilson.com>
2019-07-16 18:41:29 -0500

    wip

---
+++ WrenchIt/Data/MachinePartRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WrenchIt.Models;
+
+namespace WrenchIt.Data
+{
+    public class MachinePartRepository
+    {
+        readonly string _connectionString;
+
+        public MachinePartRepository(IOptions<DbConfiguration> dbConfig)
+        {
+            _connectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public MachinePart AddMachinePart(int machineId, int partId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var newMachinePart = db.QueryFirstOrDefault<MachinePart>(@"
+                    insert into machineparts (machineId, partId)
+                    output inserted.*
+                    values (@machineId, @partId)",
+                    new { machineId, partId });
+
+                if(newMachinePart != null)
+                {
+                    return newMachinePart;
+                }
+
+                throw new Exception("No MachinePart Created.");
+            }
+        }
+    }
+}
---
+++ WrenchIt/Models/MachinePart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WrenchIt.Models
+{
+    public class MachinePart
+    {
+        public int Id { get; set; }
+        public int MachineId { get; set; }
+        public int PartId { get; set; }
+    }
+}

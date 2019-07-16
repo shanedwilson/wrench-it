f6e9e7bb092b54d1b91e2ec6f2ce8dc4b8f46db3
Shane D Wilson <me@shanedwilson.com>
2019-07-15 23:56:54 -0500

    links adding

---
+++ WrenchIt/Controllers/LinksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WrenchIt.Data;
+using WrenchIt.Models;
+using WrenchIt.Validators;
+
+namespace WrenchIt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinksController : ControllerBase
+    {
+        readonly LinkRepository _repository;
+        readonly CreateLinkRequestValidator _validator;
+
+        public LinksController (LinkRepository repository)
+        {
+            _repository = repository;
+            _validator = new CreateLinkRequestValidator();
+        }
+
+        [HttpPost]
+        public ActionResult AddLink(CreateLinkRequest createRequest)
+        {
+            if (_validator.Validate(createRequest))
+            {
+                return BadRequest(new { error = "Please Enter All Fields" });
+            }
+
+            var newLink = _repository.AddLink(createRequest.Name, createRequest.Url);
+
+            return Created($"api/links/{ newLink.Id }", newLink);
+        }
+    }
+}
---
+++ WrenchIt/Data/LinkRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WrenchIt.Models;
+
+namespace WrenchIt.Data
+{
+    public class LinkRepository
+    {
+        readonly String _connectionString;
+
+        public LinkRepository(IOptions<DbConfiguration> dbConfig)
+        {
+            _connectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public Link AddLink(string name, string url)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var newLink = db.QueryFirstOrDefault<Link>(@"
+                    insert into links (name, url)
+                    output inserted.*
+                    values(@name, @url)",
+                    new { name, url });
+
+                if(newLink != null)
+                {
+                    return newLink;
+                }
+                throw new Exception("No Link Created");
+            }
+
+        }
+    }
+}

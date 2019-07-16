c9d5397951bd8a7c5afc3902b7436901e2c9e84b
Shane D Wilson <me@shanedwilson.com>
2019-07-15 23:21:37 -0500

    created links models

---
+++ WrenchIt/Models/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WrenchIt.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Url { get; set; }
+    }
+}

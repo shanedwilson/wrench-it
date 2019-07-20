4c77693a0087c8bbc63fee31666327babdc57368
Shane D Wilson <me@shanedwilson.com>
2019-07-20 13:26:23 -0500

    added buttons to machine card

--- WrenchIt/ClientApp/src/components/pages/MyGarage/MyGarage.js
+++ WrenchIt/ClientApp/src/components/pages/MyGarage/MyGarage.js
@@ -68,18 +68,26 @@ class MyGarage extends React.Component{
                 return (
                         <div className="d-flex justify-content-center">
                             <div className="machine-card border border-dark rounded animated fadeIn w-50 mt-5 text-center" id={selectedMachine.id}>
-                            <h3 className="text-center profile-header">{selectedMachine.year} {selectedMachine.make} {selectedMachine.model} {selectedMachine.trim}</h3>
-                            <div className="ml-1">Oil Type: {selectedMachine.oilType}</div>
-                            <div className="ml-1">Oil Quantity: {selectedMachine.oilQuantity} Quarts</div>
-                            <div className="ml-1">Tire Size: {selectedMachine.tireSize}</div>
-                            <div className="ml-1">Tire Pressure: {selectedMachine.tirePressure}</div>
-                            <div className="ml-1">Service Interval: {selectedMachine.serviceInterval}</div>
+                                <h3 className="text-center profile-header">{selectedMachine.year} {selectedMachine.make} {selectedMachine.model} {selectedMachine.trim}</h3>
+                                <div className="ml-1">Oil Type: {selectedMachine.oilType}</div>
+                                <div className="ml-1">Oil Quantity: {selectedMachine.oilQuantity} Quarts</div>
+                                <div className="ml-1">Tire Size: {selectedMachine.tireSize}</div>
+                                <div className="ml-1">Tire Pressure: {selectedMachine.tirePressure}</div>
+                                <div className="ml-1">Service Interval: {selectedMachine.serviceInterval}</div>
+                                <button id='machine-edit' type="button" className="bttn-pill profile-edit-btn ml-2" onClick={this.editMachine} title="Edit Machine">
+                                    <i className="far fa-edit fa-1x"/>
+                                </button>
+                                <button id='machine-delete' type="button" className="bttn-pill delete-btn ml-2 mr-2" onClick={this.deleteMachine} title="Delete Machine">
+                                    <i className="profile-delete-btn fas fa-trash fa-1x"></i>
+                                </button>
                             </div>
+
                         </div>
                   );
             }
             return<div></div>
         }
+
         return(
             
             <div className="myGarage mx-auto">

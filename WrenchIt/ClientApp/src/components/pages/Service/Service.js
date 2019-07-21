5c90ac0d7b0129fbfb5178bf513fa360fb971315
Shane D Wilson <me@shanedwilson.com>
2019-07-21 11:43:28 -0500

    single machine saving to state

--- WrenchIt/ClientApp/src/components/pages/Service/Service.js
+++ WrenchIt/ClientApp/src/components/pages/Service/Service.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import machineRequests from '../../../helpers/data/machineRequests';
 
 class Service extends React.Component{
+    serviceMounted = false;
+
+    state = {
+        selectedMachine: {},
+    }
+
+    getSingleMachineById = (id) => {
+        machineRequests.getSingleMachine(id)
+        .then((machine) => {
+            this.setState({ selectedMachine: machine.data })
+        })
+    }
+
+    componentDidMount(){
+        const { currentUser } = this.props;
+        this.serviceMounted = !!currentUser.id;
+        const machineId = this.props.match.params.id
+
+        console.log(machineId);
+
+        if (this.serviceMounted) {
+            this.getSingleMachineById(machineId * 1);
+        }
+    }
+
     render(){
         return(
             <div>Service Page</div>
